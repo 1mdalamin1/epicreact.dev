339603d7648a7c0ae170c90319399d9db178a0b0
Tanvir md al amin <01mdalamin1@gmail.com>
2024-11-17 18:15:18 +0600

    start react hooks

--- info.php
+++ info.php
@@ -1,8 +1,15 @@
 <?php
 /*
+setup process
+--------------------
+it clone https://github.com/kentcdodds/react-hooks.git  
+cd react-hooks  
+node setup
+npm start
+___________________________
 1. Done
 2. Done | React Fundamentals (https://github.com/kentcdodds/react-fundamentals)
-3. Wait... | React Hooks (https://github.com/kentcdodds/react-fundamentals)
+3. Wait... | React Hooks (https://github.com/kentcdodds/react-hooks.git)
 4. Pending | 
 
 
