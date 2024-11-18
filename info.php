ce15dfb324f2bd470e58ffea2211041e9df7004f
1mdalamin1 <52990610+1mdalamin1@users.noreply.github.com>
2024-11-18 19:00:09 +0600

    need to know React Patterns

--- info.php
+++ info.php
@@ -8,13 +8,18 @@ it clone https://github.com/kentcdodds/react-hooks.git
 cd react-hooks  
 node setup
 npm start
+--------------------
+cd advanced-react-hooks  
+npm run setup --silent
 ==============
 docker-compose up | If you are still unable to fix issues and you know how to use Docker 🐳 you can setup the project with the following command:
 ___________________________
 1. Done
 2. Done | React Fundamentals (https://github.com/kentcdodds/react-fundamentals)
-3. Wait... | 🎣 React Hooks 🚀 EpicReact.Dev (https://github.com/kentcdodds/react-hooks.git) 
-4. Pending | 
+3. Done | 🎣 React Hooks 🚀 EpicReact.Dev (https://github.com/kentcdodds/react-hooks.git) 
+4. Done | 🔥 Advanced React Hooks 🚀 git clone https://github.com/kentcdodds/advanced-react-hooks.git  
+5. Wait... | 💪 Advanced React Patterns 🤯 git clone https://github.com/kentcdodds/advanced-react-patterns.git  
+6. Pending | ⚡ React Performance 🚀 git clone https://github.com/kentcdodds/react-performance.git  
 
 hook-flow.png (need again stardy) | => https://github.com/donavon/hook-flow | https://www.epicreact.dev/modules/react-hooks-v1/hooks-flow
 
